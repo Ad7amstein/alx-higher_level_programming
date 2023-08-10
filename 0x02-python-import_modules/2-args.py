598ad94c6f5bb8abc9736c33bb7dc2b55772b73c
Adham <adham.3llam@gmail.com>
2023-08-10 09:06:22 -0700

    print arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    length = len(sys.argv) - 1
+    print("{} {}".format(length, "argument" if length == 1
+        else "argumetns"), end='')
+    if length == 0:
+        print(".")
+    else:
+        print(":")
+        for i in range(1, length + 1):
+            print("{}: {}".format(i, sys.argv[i]))
