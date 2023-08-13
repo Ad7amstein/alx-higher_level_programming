a787020dea72118f052a3f493c4600c9fa3c1164
Adham <adham.3llam@gmail.com>
2023-08-13 08:11:23 -0700

    print list info

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,23 +1,21 @@
-#include <Python.h>
-#include <stdio.h>
+#include "Python.h"
 /**
  * print_python_list_info - frees a listint_t list
  * @p: pointer to list to be freed
  */
 void print_python_list_info(PyObject *p)
 {
-	PyListObject *list = (PyListObject *)p;
-	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t size = PyList_Size(p), i;
 	const char *type_name;
 	PyObject *element;
 
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated  = %ld\n", list->allocated);
+	printf("[*] Allocated  = %ld\n", ((PyListObject *)p)->allocated);
 
-	for (Py_ssize_t i = 0; i < size; ++i)
+	for (i = 0; i < size; ++i)
 	{
-		element = PyList_GetItem(p, i);
-		type_name = Py_TYPE(element)->tp_name;
+		element = PyList_GET_ITEM(p, i);
+		type_name = element->ob_type->tp_name;
 		printf("Element %ld: %s\n", i, type_name);
 	}
 }
