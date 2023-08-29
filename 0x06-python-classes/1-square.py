cba3e89b595028d089e2728f43647d075440a421
3llam <adham32003200@gmail.com>
2023-08-29 09:50:59 +0300

    modify style

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Defines a class Square."""
+
+
 class Square:
     """Represents a square, withe a size."""
 
     def __init__(self, size):
-        """Initializes the data."""
+        """Initializes the data.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
