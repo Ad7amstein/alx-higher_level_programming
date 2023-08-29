e2084644f87db51cb4f3a56e836b397cd48c38f0
3llam <adham32003200@gmail.com>
2023-08-30 00:34:35 +0300

    Overload comparison operators

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -24,7 +24,7 @@ class Square:
 
     @size.setter
     def size(self, size):
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
@@ -32,84 +32,84 @@ class Square:
             self.__size = size
 
     def __lt__(self, other):
-        """Check if the current object's size 
+        """Check if the current object's size
         is less than the size of another object.
 
         Args:
             other: Another object to compare the size against.
 
         Returns:
-            bool: True if the current object's size 
+            bool: True if the current object's size
             is less than the size of the other object,
                   False otherwise.
         """
         return self.__size < other.size
 
     def __le__(self, other):
-        """Check if the current object's size 
+        """Check if the current object's size
         is less than or equal the size of another object.
 
         Args:
             other: Another object to compare the size against.
 
         Returns:
-            bool: True if the current object's size 
+            bool: True if the current object's size
             is less than or equal the size of the other object,
                   False otherwise.
         """
         return self.__size <= other.size
 
     def __eq__(self, other):
-        """Check if the current object's size 
+        """Check if the current object's size
         is equal to the size of another object.
 
         Args:
             other: Another object to compare the size against.
 
         Returns:
-            bool: True if the current object's size 
+            bool: True if the current object's size
             is equal to the size of the other object,
                   False otherwise.
         """
         return self.__size == other.size
 
     def __ne__(self, other):
-        """Check if the current object's size 
+        """Check if the current object's size
         is not equal to the size of another object.
 
         Args:
             other: Another object to compare the size against.
 
         Returns:
-            bool: True if the current object's size 
+            bool: True if the current object's size
             is not equal to the size of the other object,
                   False otherwise.
         """
         return self.__size != other.size
 
     def __gt__(self, other):
-        """Check if the current object's size 
+        """Check if the current object's size
         is greater than the size of another object.
 
         Args:
             other: Another object to compare the size against.
 
         Returns:
-            bool: True if the current object's size 
+            bool: True if the current object's size
             is greater than the size of the other object,
                   False otherwise.
         """
         return self.__size > other.size
 
     def __ge__(self, other):
-        """Check if the current object's size 
+        """Check if the current object's size
         is greater than or equal the size of another object.
 
         Args:
             other: Another object to compare the size against.
 
         Returns:
-            bool: True if the current object's size 
+            bool: True if the current object's size
             is greater than or equal the size of the other object,
                   False otherwise.
         """
