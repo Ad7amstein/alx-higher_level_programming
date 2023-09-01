2396cac61c8a0ef76290f78883b135aa59d48a6c
3llam <adham32003200@gmail.com>
2023-09-01 08:44:32 +0300

    remove the loop => let's do it in a pythonic way :)

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -17,24 +17,22 @@ def matrix_divided(matrix, div):
 
     Returns: The matrix after the division.
     """
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    if not isinstance(div, int) and not isinstance(div, float):
-        raise TypeError("div must be a number")
-
     if (not isinstance(matrix, list) or matrix == [] or
         not all(isinstance(row, list) for row in matrix) or
         not all(isinstance(num, int) or isinstance(num, float)
                 for num in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists) \
                         of integers/floats")
+
     row_len = len(matrix[0])
     if (not all(len(row) == row_len for row in matrix)):
         raise TypeError("Each row of the matrix must have the same size")
-    new_matrix = []
-    for i in range(len(matrix)):
-        new_matrix.append(list(matrix[i]))
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = round(new_matrix[i][j] / div, 2)
 
-    return new_matrix
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return (list(map(lambda row: list(map(lambda x: round(x / div, 2), row)),
+                     matrix)))
