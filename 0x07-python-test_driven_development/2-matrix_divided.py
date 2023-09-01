32379f177d72ffd0c626d8b287fe04fd5dd34463
3llam <adham32003200@gmail.com>
2023-09-01 08:30:07 +0300

    add TypeError check for numbers befor the for

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -22,8 +22,10 @@ def matrix_divided(matrix, div):
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
-    if not isinstance(matrix, list) or matrix == [] or \
-            not all(isinstance(row, list) for row in matrix):
+    if (not isinstance(matrix, list) or matrix == [] or
+        not all(isinstance(row, list) for row in matrix) or
+        not all(isinstance(num, int) or isinstance(num, float)
+                for num in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists) \
                         of integers/floats")
     row_len = len(matrix[0])
@@ -34,9 +36,6 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         new_matrix.append(list(matrix[i]))
         for j in range(len(matrix[i])):
-            if not isinstance(matrix[i][j], int) and not isinstance(matrix[i][j], float):
-                raise TypeError("matrix must be a matrix (list of lists) \
-                                of integers/floats")
             new_matrix[i][j] = round(new_matrix[i][j] / div, 2)
 
     return new_matrix
