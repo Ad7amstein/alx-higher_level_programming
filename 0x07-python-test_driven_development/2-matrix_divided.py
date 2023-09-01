f5af09376dd8d252873114bc8d42fb66a1cd7903
3llam <adham32003200@gmail.com>
2023-09-01 08:33:05 +0300

    add the lenght check befor the loo[

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -29,11 +29,10 @@ def matrix_divided(matrix, div):
         raise TypeError("matrix must be a matrix (list of lists) \
                         of integers/floats")
     row_len = len(matrix[0])
-
+    if (not all(len(row) == row_len for row in matrix)):
+        raise TypeError("Each row of the matrix must have the same size")
     new_matrix = []
     for i in range(len(matrix)):
-        if len(matrix[i]) != row_len:
-            raise TypeError("Each row of the matrix must have the same size")
         new_matrix.append(list(matrix[i]))
         for j in range(len(matrix[i])):
             new_matrix[i][j] = round(new_matrix[i][j] / div, 2)
