ab27a898885b6831b96f94dfe48642cfb38d1fa5
3llam <adham32003200@gmail.com>
2023-09-05 01:30:29 +0300

    calculate the perimeter of the rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defines a rectangle class."""
+
+
+class Rectangle:
+    """Represents a rectangle shape."""
+
+    def __init__(self, width=0, height=0):
+        """Initializes the rectangle data."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Set/Get the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set/Get the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def perimeter(self):
+        """Returns teh perimeter of the rectangle."""
+        return (2 * self.__width + 2 * self.__height)
