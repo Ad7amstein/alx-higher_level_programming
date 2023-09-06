2e363a563ccfa69112b76cb043e1f76132c5c70b
3llam <adham32003200@gmail.com>
2023-09-06 04:47:11 +0300

    set attribute

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a LockedClass."""
+from typing import Any
+
+
+class LockedClass:
+    """Represents a LockedClass."""
+
+    def __setattr__(self, name, value):
+        """Define how setattr should work"""
+        if name == "first_name":
+            super().__setattr__(name, value)
+        else:
+            raise \
+        AttributeError("'LockedClass' object has no attribute 'last_name'")
