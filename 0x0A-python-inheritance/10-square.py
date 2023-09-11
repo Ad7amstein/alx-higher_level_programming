68c7eadd5a21fc1be41e6a3a3c100e5e03f353dd
3llam <adham32003200@gmail.com>
2023-09-11 18:03:02 +0300

    Class suqre inherits form basegeometry

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square shape."""
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
+
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Calculates and return the square area"""
+        return (self.__size * self.__size)
