c8b065fff586a42a702786762bf9eb522c069131
3llam <adham32003200@gmail.com>
2023-09-11 18:46:59 +0300

    modify init

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,7 +10,8 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initializes the data."""
-        super().__init__(size, size)
-
+        super().integer_validator("size", size)
         self.__size = size
+
+        super().__init__(self.__size, self.__size)
 
