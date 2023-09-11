5baa0aeb2fa944e3c49d49eff678df8e4e6a8d28
3llam <adham32003200@gmail.com>
2023-09-11 18:09:42 +0300

    add docstr

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -19,4 +19,5 @@ class Square(Rectangle):
         return (self.__size * self.__size)
 
     def __str__(self):
+        """String representation of the object."""
         return ("[Square] {}/{}".format(self.__size, self.__size))
