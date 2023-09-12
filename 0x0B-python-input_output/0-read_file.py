4387d10a872731ae592422bf1c3e8a4004dc9a1e
3llam <adham32003200@gmail.com>
2023-09-12 12:14:13 +0300

    modify encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, "r", encoding="UTF-8") as MyFile:
-        print(MyFile.read())
+    with open(filename, "r", encoding="utf-8") as MyFile:
+        print(MyFile.read(), end="")
