6b783c6881f51a2a5b7b5a89de7dfed744d2d68a
3llam <adham32003200@gmail.com>
2023-09-13 01:42:45 +0300

    reload from json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a student class."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of the data."""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        d = dict()
+        if (type(attrs) is list
+                and all(type(x) is str for x in attrs)):
+            d = {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        else:
+            d = self.__dict__
+        return d
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance."""
+        for k, v in json.items():
+            setattr(self, k, v)
