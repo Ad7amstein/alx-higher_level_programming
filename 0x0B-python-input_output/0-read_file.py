afd482d88af6e1dc697b3ba4b45d300f3798df4b
3llam <adham32003200@gmail.com>
2023-09-12 12:04:10 +0300

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a read file function."""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, "r", encoding="UTF-8") as MyFile:
+        print(MyFile.read())
