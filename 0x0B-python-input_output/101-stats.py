59388b59f42976c7f8a93e7ffa29248ebd11c790
3llam <adham32003200@gmail.com>
2023-09-13 02:58:04 +0300

    status codes

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Script that reads stdin line by line and computes metrics."""
+
+import sys
+
+
+def print_info(size, d):
+    """Prints information."""
+    print("File size: {}".format(size))
+    for k, v in sorted(d.items()):
+        if v > 0:
+            print("{}: {}".format(k, v))
+
+
+if __name__ == "__main__":
+    status_codes = {"200": 0, "301": 0,
+                    "400": 0, "401": 0,
+                    "403": 0, "404": 0,
+                    "405": 0, "500": 0}
+
+    counter = 1
+    total_size = 0
+    for l in sys.stdin:
+        line = l.split()
+        total_size += line[-1]
+        code = line[-2]
+
+        if hasattr(status_codes, code):
+            status_codes[code] += 1
+
+        if counter == 10:
+            print_info(total_size, status_codes)
+            counter = 0
+
+        counter += 1
+
+    print_info(total_size, status_codes)
