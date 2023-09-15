2061233ae8cd165ae8757f41c8e42b612d4b131b
3llam <adham32003200@gmail.com>
2023-09-16 00:20:01 +0300

    create new inctance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,3 +44,16 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set."""
+        from models.rectangle import Rectangle
+        from models.square import Square
+
+        if cls.__name__ == 'Rectangle':
+            obj = Rectangle(1, 1)
+        if cls.__name__ == 'Square':
+            obj = Square(1)
+        obj.update(**dictionary)
+        return obj
