e77eb65c95f11eccbc9fcab662b2bde99d0239a7
3llam <adham32003200@gmail.com>
2023-09-17 02:10:55 +0300

    test base

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines Unittest for base module."""
+
+import unittest
+import pycodestyle
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """Test suits for base module functiosn."""
+
+    def test_pycodestyle():
+        """Test that we conform to PEP-8."""
+        style = pycodestyle.StyleGuide(quit=True)
+        result = style.check_files('base.py')
