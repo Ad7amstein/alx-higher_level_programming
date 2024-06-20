d61423030f22b55e9fc6462c3d00740f0e974d02
Ad7amstein <adham32003200@gmail.com>
2024-06-19 20:14:51 -0700

    Fix convert to Number bug

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,17 +4,19 @@ const argv = process.argv;
 if (argv.length <= 3) {
   console.log(0);
 } else {
-  let mx = argv[2];
+  let mx = Number(argv[2]);
   argv.forEach((val, index) => {
-    if (val > mx) {
-      mx = val;
+    const num1 = Number(val);
+    if (num1 > mx) {
+      mx = num1;
     }
   });
 
   let curMx = -Infinity;
   argv.forEach((val, index) => {
-    if (val > curMx && val < mx) {
-      curMx = val;
+    const num2 = Number(val);
+    if (num2 > curMx && num2 < mx) {
+      curMx = num2;
     }
   });
 
