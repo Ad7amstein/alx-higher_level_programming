30fa63974768d4080cf59a111ab2d1c8b4ca5751
Ad7amstein <adham32003200@gmail.com>
2024-06-19 17:11:23 -0700

    fix a typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const len = argv.length;
 if (len === 2) {
   console.log('No argument');
 } else if (len === 3) {
-  console.log('Argumet found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
