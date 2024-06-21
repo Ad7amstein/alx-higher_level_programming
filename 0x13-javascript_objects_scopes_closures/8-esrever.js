620e3a03e64ac2ebef56c6a7a8ed2bb199bd13e4
Ad7amstein <adham32003200@gmail.com>
2024-06-20 17:23:12 -0700

    Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const newList = [];
+  const len = list.length;
+  for (let i = len - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
+};
