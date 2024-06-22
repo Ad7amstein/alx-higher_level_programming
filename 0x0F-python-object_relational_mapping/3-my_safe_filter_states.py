c64b722fc3090cf56abb9299e3a2aaeef98e8789
Ad7amstein <adham32003200@gmail.com>
2024-06-22 00:36:05 -0700

    pass parameters as a tuple

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,16 +10,16 @@ def main():
     displays all values in the states table of hbtn_0e_0_usa
     where name matches the argument.
     """
-    user, password, database, searche_name =\
+    user, password, database, searched_name =\
         sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
     host = "localhost"
-    prot = 3306
+    port = 3306
     db = MySQLdb.connect(host=host, port=port,
                          user=user, passwd=password, db=database)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
-                   WHERE states.name = BINARY ?
-                   ORDER BY states.id ASC""", searched_name)
+                   WHERE states.name = %s
+                   ORDER BY states.id ASC""", (searched_name,))
     rows = cur.fetchall()
     for row in rows:
         print(row)
