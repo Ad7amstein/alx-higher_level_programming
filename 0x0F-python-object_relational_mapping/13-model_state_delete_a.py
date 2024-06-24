cf3da6947c4a76080eb5960064f9b50767056e93
Ad7amstein <adham32003200@gmail.com>
2024-06-23 21:27:07 -0700

    Edit style

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     session = Session(bind=engine)
     removed_states = session.query(State).filter(State.name.like('%a%')).\
-            order_by(State.id).all()
+        order_by(State.id).all()
     for state in removed_states:
         session.delete(state)
     session.commit()
