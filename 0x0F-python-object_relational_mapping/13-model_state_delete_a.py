f3302eb97997c6f822b41258f21eb715f5623a31
Ad7amstein <adham32003200@gmail.com>
2024-06-23 21:26:22 -0700

    Order States!

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     session = Session(bind=engine)
-    removed_states = session.query(State).filter(State.name.like('%a%')).all()
+    removed_states = session.query(State).filter(State.name.like('%a%')).\
+            order_by(State.id).all()
     for state in removed_states:
         session.delete(state)
     session.commit()
