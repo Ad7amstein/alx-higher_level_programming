cf6be9c1be1f336396385431bee51184438b489f
Ad7amstein <adham32003200@gmail.com>
2024-07-08 06:02:46 -0700

    find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Find a peak
+
+def find_peak(nums=None):
+    """Finds a peak in a list of numbers
+    """
+    if not nums or len(nums) == 0:
+        return None
+    if len(nums) == 1:
+        return nums[0]
+    if len(nums) >= 2:
+        if nums[0] >= nums[1]:
+            return nums[0]
+        if nums[len(nums) - 1] >= nums[len(nums) - 2]:
+            return nums[len(nums) - 1]
+    for i in range(1, len(nums)-1):
+        if nums[i - 1] <= nums[i] >= nums[i + 1]:
+            return nums[i]
+    return None
