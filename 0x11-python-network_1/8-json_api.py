8da7747390552fc48e469cf363507daf61cd8c68
Ad7amstein <adham32003200@gmail.com>
2024-07-08 22:37:05 -0700

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Search API
+"""
+
+import sys
+import requests
+
+
+def main():
+    """Main program
+    """
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        val = sys.argv[1]
+    except IndexError:
+        val = ""
+
+    req = requests.post(url, data={'q': val})
+    try:
+        data = req.json()
+    except requests.exceptions.JSONDecodeError:
+        if req.status_code == 204:
+            print("No result")
+        else:
+            print("Not a valid JSON")
+    if not data or len(data) < 1:
+        print("No result")
+    else:
+        print(f"[{data['id']}] {data['name']}")
+
+
+if __name__ == "__main__":
+    main()
