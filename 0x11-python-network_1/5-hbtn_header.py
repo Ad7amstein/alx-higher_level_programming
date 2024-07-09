280503be9466e6fdced647ce30b93cff7d57a6bd
Ad7amstein <adham32003200@gmail.com>
2024-07-08 22:07:58 -0700

    Response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Response header value 1
+"""
+
+import sys
+import requests
+
+
+def main():
+    """Main program
+    """
+    req = requests.get(sys.argv[1])
+    print(req.headers['X-Request-Id'])
+
+
+if __name__ == "__main__":
+    main()
