644e8fba45aa42787d539c439c929094efe957f2
Ad7amstein <adham32003200@gmail.com>
2024-07-09 07:59:30 +0300

    add docs

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,7 @@
 import requests
 
 def main():
+    """Fetches https://alx-intranet.hbtn.io/status"""
     url = "https://alx-intranet.hbtn.io/status"
     req = requests.get(url)
     print("Body response:")
